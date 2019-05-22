52e9e14e212903673c2bf3ed465c69755da294ce
Sachin Kumar <sachin.kumar@optym.com>
2019-05-22 12:20:22 +0530

    First

---
+++ SignalRClient/SignalRClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hubConnection = new HubConnection("http://localhost:8080");
+            var hubProxy = hubConnection.CreateHubProxy("haulPlanHub");
+            await hubConnection.Start();
+            await hubProxy.Invoke("ReportEngineCompletion",476, 0);
+            
+            Console.ReadLine();
+        }
+    }
+}
